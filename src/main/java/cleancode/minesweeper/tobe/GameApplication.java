f95fbc5f3597fd0f47c75ee54c5aca447cf82ceb
kkwon_dae <eodud4976@gmail.com>
2024-10-30 23:36:37 +0900

    2024-10-30

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,9 +1,18 @@
 package cleancode.minesweeper.tobe;
 
+import cleancode.minesweeper.tobe.gamelevel.Advanced;
+import cleancode.minesweeper.tobe.gamelevel.GameLevel;
+import cleancode.minesweeper.tobe.gamelevel.Middle;
+import cleancode.minesweeper.tobe.gamelevel.VeryBeginner;
+
 public class GameApplication {
 
     public static void main(String[] args) {
-        Minesweeper minesweeper = new Minesweeper();
+//        GameLevel gameLevel = new VeryBeginner();
+//        GameLevel gameLevel = new Beginner();
+//        GameLevel gameLevel = new Middle();
+        GameLevel gameLevel = new Advanced();
+        Minesweeper minesweeper = new Minesweeper(gameLevel);
         minesweeper.run();
     }
 
